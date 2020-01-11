43d91cb4358f93686824397d0c451e833e307376
Manuel Pfemeter <manne@users.noreply.github.com>
2020-01-11 13:36:28 +0100

    Validate input

--- src/Application/TrivialController.cs
+++ src/Application/TrivialController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Application
@@ -6,10 +8,34 @@ namespace Application
     [Route("api/[controller]")]
     public class TrivialController
     {
-        [HttpGet]
-        public IEnumerable<string> Get()
+        [HttpGet("{name}")]
+        public IEnumerable<string> Get(TrivialOptions options)
         {
-            return new string[] { "nothing special" };
+            if (options.PageNumber == 21)
+            {
+                throw new InvalidOperationException("oops");
+            }
+
+            return new string[] { "nothing special", options.Name, options.PageNumber.ToString() };
+        }
+    }
+
+    public sealed class TrivialOptions
+    {
+        public string Name { get; set; }
+
+        public int PageNumber { get; set; }
+    }
+
+    public class TrivialOptionsValidator : AbstractValidator<TrivialOptions>
+    {
+        public TrivialOptionsValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty();
+            RuleFor(x => x.PageNumber)
+                .InclusiveBetween(0, 21)
+                .WithMessage("Please specify a first name");
         }
     }
 }
