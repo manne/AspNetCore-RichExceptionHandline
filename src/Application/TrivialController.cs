6cf29041297701d7d5697db01a37e7b46931f599
Manuel Pfemeter <manne@users.noreply.github.com>
2020-01-11 13:10:01 +0100

    Add trivial routing

---
+++ src/Application/TrivialController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application
+{
+    [Route("api/[controller]")]
+    public class TrivialController
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "nothing special" };
+        }
+    }
+}
